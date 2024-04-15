a5dc59a11f5fd9d7564fef25dac0329ab2a9b240
Oriol Pujade <uripujade@gmail.com>
2024-04-15 13:22:52 +0200

    Started React App

---
+++ src/PrimerComponente.jsx
@@ -0,0 +1,15 @@
+const string = 'Esto es un texto'
+const number = 123456
+const array = ['Curso de React', 'YouTube', 4, 1000000]
+const boolean = true
+const funcion = () => 1 + 1
+const objeto = { nombre: 'Curso de Javascript', duracion: 4 }
+const fecha = new Date()
+
+export const PrimerComponente = () => {
+    return (
+        <>
+            <h1>{ }</h1>
+        </>
+    )
+}
